b672c6e7bca40020615c94ed83bf04713a37635b
atljeremy <atljeremy@me.com>
2012-06-25 19:46:25 -0400

    Initial Commit

---
+++ aoc1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  aoc1
+//
+//  Created by Jeremy Fox on 6/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

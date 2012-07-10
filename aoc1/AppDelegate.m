a7a7c6d0fc4d7512eef31e0c9874353cc7a01646
Jeremy Fox <atljeremy@me.com>
2012-07-10 19:51:24 -0400

    added comments for this weeks requirements

--- aoc1/AppDelegate.m
+++ aoc1/AppDelegate.m
@@ -118,19 +118,36 @@
         iGotAnotherTicket++;
     }
     
+    //Change the background color of the view to any color you wish.
+    
+    // Book Information and UILabels
+    // Create a UILabel and place it at the top of the view. This is used as the title of the book. Center the text for this label.
+    // Create another label that contains the text "Author:" with the text right justified.
+    // Create a UILabel to the right of the author title label and have it contain the name of the author of the book you chose. This text will be left justified.
+    // Create a UILabel on the next line called "Published:". This text is right justified.
+    // Create a label next to the Published label and add the text of when the book was published. This text is left justified.
+    // Create a UILabel with the text "Summary". This text is left justified.
+    // Create another UILabel that contains a small summary of the book's plot. This text is centered and should span multiple lines.
+    
+    // Labels and Arrays
+    // Create an NSArray of 5 items talked about in the book. These items will be NSStrings. Add the items to the array.
+    // Create a variable of type NSMutableString and allocate it. Loop through the NSArray you created and append each of these items to your NSMutableString separated by commas. (For example: dinosaurs, jeeps, storm, giant turkeys, eating people)
+    // Create a label with the text "List of items" and add it to the parent view. Make sure the text is left justified.
+    // Create another label beneath and set the text to the NSMutableString text. Increase the number of lines if necessary. Make sure the text in this label is centered
+    
     //***************************************************************
     // Standard (boilerplate) code to create and load a new window
     //
-    // I added a rootViewController so the app would stop complaing 
+    // I added a rootViewController so the app would stop complaining 
     // about not having a root view controller at the end of 
     // application launch.
     //***************************************************************
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor brownColor];
     
-    rootView      = [[UIViewController alloc] init];
+    rootView       = [[UIViewController alloc] init];
     rootView.title = @"Jeremy Fox - AOC1";
-    navController = [[UINavigationController alloc] initWithRootViewController:rootView];
+    navController  = [[UINavigationController alloc] initWithRootViewController:rootView];
     
     self.window.rootViewController = navController;
     [self.window makeKeyAndVisible];
